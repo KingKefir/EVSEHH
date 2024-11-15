73838e0bf7ede1175d85966b5e6ac62d92a37a8f
RMN <rmn@users.noreply.github.com>
2024-11-15 12:41:57 +0100

    low_memory=false

--- mypages/sub_p2.py
+++ mypages/sub_p2.py
@@ -4,18 +4,11 @@ import plotly.graph_objects as go
 import warnings
 import streamlit as st
 
-# Daten laden
-data = pd.read_csv("data\\ladesaeulenregister.csv", delimiter=";", on_bad_lines="skip")
-# Unnötige Spalten entfernen
-data = data.drop(columns=['Adresszusatz', 'Public Key1', 'Public Key2', 'Public Key3', 'Public Key4' ])
-data = data.dropna(subset=['Inbetriebnahmedatum'])
-data = data.dropna(subset=['Betreiber'])
-
 def app():
     st.title("Erster Ansatz: Analyse mit SQL und Tableau")
 
     # Pfad zur PNG-Datei
-    image_path = "ERD.png"
+    image_path = "data\\ERD.png"
 
     # Bild anzeigen
     st.image(image_path, caption="ERD der Datenbank", use_column_width=True)
