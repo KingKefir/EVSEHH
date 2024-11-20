df60f48f5d7e0be0f8d18e17ad5bb43f8d6805b9
RMN <rmn@users.noreply.github.com>
2024-11-20 18:01:48 +0100

    docker tag 'latest' hinzugefügt

--- mypages/dankebye.py
+++ mypages/dankebye.py
@@ -8,8 +8,8 @@ def app():
     code = '''git clone https://github.com/KingKefir/EVSEHH.git'''
     st.code(code, language='bash')
 
-    codedocker = '''docker pull kefirconnoisseur/evse_analysis:0.6
+    codedocker = '''docker pull kefirconnoisseur/evse_analysis:latest
 
-docker run -p 8501:8501 kefirconnoisseur/evse_analysis:0.6'''
+docker run -p 8501:8501 kefirconnoisseur/evse_analysis:latest'''
     st.code(codedocker, language='bash')
     st.image("data/plug-e.png", caption="Quelle: openai/Dall-E")
